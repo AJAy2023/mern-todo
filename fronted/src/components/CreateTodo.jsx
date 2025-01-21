72a8cdd5dec96a2f67b221e90263a227e8ec4428
Ajay <ajayjadhav0913@gmail.com>
2025-01-21 23:02:00 +0530

    completed todo uisng mernstack

--- fronted/src/components/CreateTodo.jsx
+++ fronted/src/components/CreateTodo.jsx
@@ -1,17 +1,43 @@
-export function CreateTodo(){
+import { useState } from "react";
+
+export function CreateTodo(props){
+  const [title , setTitle] = useState("");
+  const [description , setDescription] = useState("");
     return <div>
-      <input style={{
+      <input id="title" style={{
         padding:10,
         margin:10
       }} type="text" placeholder="title" /> <br />
       <input style={{
         padding:10,
         margin:10
-      }} type="text" placeholder="description" /> <br />
+      }} type="text" placeholder="description" onChange={function(e)
+        {
+          const value  = e.target.value
+          setDescription(e.target.value);
+        }
+      } /> <br />
       <button
       style={{
         padding:10,
         margin:10
+      }} onClick={()=>{
+        // if you have use the fetch then u have to make the json.stringify 
+        fetch("http://localhost:3000/todos",{
+          method: "POST",
+          body : JSON.stringify({
+            title : title,
+            description : description
+          }),
+          headers :{
+            "content-type":"application/json"
+          }
+        }).then(async function(res)
+        {
+          const json  = await res.json();
+          setTodos(json.todos);
+          alert("Todo added");
+        })
       }}>add a todo </button>
         </div>
 }
